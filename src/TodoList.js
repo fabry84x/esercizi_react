cd379aa71a20b8f478dbd221b6c6d1ff1abd6c30
Fabrizio <fabrizio.boschetto@email.it>
2022-06-17 12:09:22 +0200

    feature/Lists-03

---
+++ src/TodoList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export class TodoList extends React.Component {
+
+    state={
+        items:["item1","item2","item3"],
+        inputItem:""
+    }
+
+    handleInputChange = (event) => {
+        const value = event.target.value
+        this.setState({
+            inputItem:value
+        })
+    }
+
+    handleListItem = () => {
+        const list = this.state.items
+        list.push(this.state.inputItem)
+        this.setState({list})
+    }
+
+    render() {
+        const listItems = this.state.items.map((listItem,index)=><li key={index}>{listItem}</li>)
+        return(
+            <div>
+                <ul>{listItems}</ul>
+                <input name="inputItem" value={this.state.inputItem} onChange={this.handleInputChange}/>
+                <button onClick={this.handleListItem}>Add item</button>
+            </div>
+        )
+    }
+}
