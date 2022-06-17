df0ae69334d11aa362555a4765dc8b46b97963fa
Fabrizio <fabrizio.boschetto@email.it>
2022-06-17 20:47:24 +0200

    feature/Lists-06

--- src/TodoList.js
+++ src/TodoList.js
@@ -29,14 +29,22 @@ export class TodoList extends React.Component {
         })
     }
 
+    removeItem = (i) => {
+        const newList = this.state.items.splice(i,1)
+        this.setState({
+            newList
+        })
+    }
+
     render() {
-        const listItems = this.state.items.map((listItem,index)=><li key={index}>{listItem}</li>)
+        const listItems = this.state.items.map((listItem,index)=><li key={index}>{listItem}<button onClick={() => this.removeItem(index) }>Delete</button></li>)
         return(
             <div>
                 <ul>{listItems}</ul>
                 <input name="inputItem" value={this.state.inputItem} onChange={this.handleInputChange}/>
                 <button onClick={this.handleListItem}>Add item</button>
                 <button onClick={this.handleResetState}>Reset</button>
+                
             </div>
         )
     }
