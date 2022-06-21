c9431bb40aa8bed54998cf2a5a41900b39bb7db1
Fabrizio <fabrizio.boschetto@email.it>
2022-06-21 19:52:11 +0200

    feature/Context-01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { Container } from "./Container";
+import { LanguageContext } from "./LanguageContext";
+import { DisplayLanguage } from "./DisplayLanguage"
 
 export class App extends React.Component {
+    state={
+        language:'en'
+    }
+
+    handleLanguageChange = (event) => {
+        this.setState ({
+            language:event.target.value
+        })
+    }
+
     render() {
         return (
-            <Container title="My App"></Container>
+            <div>
+                <select value={this.state.language} onChange={this.handleLanguageChange}>
+                    <option value='en'>ENGLISH</option>
+                    <option value='it'>ITALIANO</option>
+                </select>
+                <LanguageContext.Provider value={this.state.language}>
+                    <Container title="My App">
+                        <DisplayLanguage />
+                    </Container>
+                </LanguageContext.Provider>
+                
+            </div>
+            
         )
     }
 }
