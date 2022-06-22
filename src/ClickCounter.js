966cc6df085becd92ebbc422c6314ab20bdb6992
Fabrizio <fabrizio.boschetto@email.it>
2022-06-22 18:21:28 +0200

    feature/UseState-01

---
+++ src/ClickCounter.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export function ClickCounter({initialValue=0}) {
+
+    const [count, setCount] = useState(initialValue)
+
+    function handleCounterIncrement () {
+        setCount((c) => c + 1)
+    }
+
+
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={handleCounterIncrement}>Increment</button>
+        </div>
+    )
+
+} 
