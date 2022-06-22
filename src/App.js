b48ffa50e3058e0889d7669c004e8c331f366f18
Fabrizio <fabrizio.boschetto@email.it>
2022-06-22 11:57:20 +0200

    feature/FunctionComponents-02

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Welcome } from "./Welcome";
+import { Sum } from "./Sum"
 
 export class App extends React.Component {
   render () {
-      return <Welcome name='Cristina'/>
+      const numbersArr = [3,6,9,5,2,8]
+      let sum = 0
+      numbersArr.forEach(num =>{
+        sum+=num
+      })
+      return (
+        <div>
+          <Welcome name='Cristina'/>
+          <Sum numbers={sum}/>
+        </div>  
+      )  
   }
 } 
---
+++ src/Sum.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function Sum(props) {
+    return (
+        <h1>{props.numbers}</h1>
+    )
+}
+
