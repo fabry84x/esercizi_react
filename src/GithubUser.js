f543b87e6aedb7e841f876ea477867a5605d0255
Fabrizio <fabrizio.boschetto@email.it>
2022-06-27 19:03:26 +0200

    feature/CustomHooks-04

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,27 +2,41 @@ import { useEffect, useState } from "react"
 
 function useGithubUser(username) {
     const [data, setData] = useState()
+    const [loading, setLoading] = useState(false)
+    const [error,setError] = useState()
 
     useEffect(() => {
+        setLoading(true)
+        setError(null)
         fetch(`https://api.github.com/users/${username}`)
             .then(response => {
+                if(response.status !== 200) {
+                    setError(new Error())
+                }
                 return response.json()
             })
             .then(json => {
-                console.log(json)
-                setData(json)
+                setData(json)   
+            })
+            .catch(error=>{
+                setError(error)
+            })
+            .finally(()=>{
+                setLoading(false)
             })
     }, [username])
 
-    return data
+    return {data,loading,error}
 }
 export function GithubUser({ username }) {
 
-const data = useGithubUser(username)
+const {data,loading,error} = useGithubUser(username)
 
     return (
         <div>
-            {data && <h1>{data.name}, {data.location}</h1>}
+            {loading && <h1>Loading</h1>}
+            {error && <h1>There has been an error</h1>}
+            {data && <h1>{data.name} {data.location}</h1>}
         </div>
     )
 } 
