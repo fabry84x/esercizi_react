23736c81f6d8318200435aa13da4f70515623c48
Fabrizio <fabrizio.boschetto@email.it>
2022-06-27 19:41:02 +0200

    feature/Props-02

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Age } from "./Age";
 
 export class Welcome extends React.Component {
     render() {
         return (
             <div>    
                 <p>Welcome, {this.props.name}!</p>
-                <Age age={this.props.age} />
             </div>
         )        
     }
+}
+
+Welcome.defaultProps = {
+    name: "Daniele"
 }
