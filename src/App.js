56da2db7261935702ddccfc052b5df60f78958ab
Fabrizio <fabrizio.boschetto@email.it>
2022-06-29 18:30:39 +0200

    feature/ReactRouter-02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import {Welcome} from "./Welcome"
+import {Counter} from "./Counter"
+
 
 export function App() {
     return(
         <Routes>
             <Route path="/" element={<Welcome name='Fabio'/>} />
+            <Route path="/counter" element={<Counter />} />
         </Routes>
     )
 }
