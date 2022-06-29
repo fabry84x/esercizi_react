fc71bd887d862ab15de5fa4865331c5f906ba012
Fabrizio <fabrizio.boschetto@email.it>
2022-06-29 13:22:04 +0200

    feature/UseContext

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,24 +1,11 @@
-import React from "react";
+import { useContext } from "react";
 import { LanguageContext } from "./LanguageContext";
 
 const Strings = {
     en:{SELECTED_LANGUAGE: 'Selected language: English'},
     it:{SELECTED_LANGUAGE: 'Lingua selezionata: Italiano'}
 }
-export class DisplayLanguage extends React.Component {
-    render() {
-        return (
-            <div>
-                <LanguageContext.Consumer>
-                    {language => {
-                        return (
-                            <h1>
-                                {Strings[language].SELECTED_LANGUAGE}
-                            </h1>
-                        )
-                    }}
-                </LanguageContext.Consumer>
-            </div>
-        )    
-    }
+export function DisplayLanguage () {
+    const language = useContext(LanguageContext)
+    return <h1>{Strings[language].SELECTED_LANGUAGE}</h1>    
 }
