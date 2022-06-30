c42beb5d5ca7c8c47ef32d30d55aa4210e46141f
Fabrizio <fabrizio.boschetto@email.it>
2022-06-30 20:19:41 +0200

    feature/SWR-01

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,9 +2,15 @@ import useSWR from "swr"
 
 const fetcher = url => fetch(url).then(response => response.json())
 
-export function GithubUser({ username }) {
-
+function useGithubUser(username) {
     const { data } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    
+    return {data}
+}
+
+export function GithubUser({username}) {
+
+    const { data } = useGithubUser(username)
 
     return (
         <div>
